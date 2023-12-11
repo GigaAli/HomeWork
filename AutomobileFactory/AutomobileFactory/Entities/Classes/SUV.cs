60161bd91f0c3d10d53cdcc5bfbc662e3ea8ad23
Ali <Ali.shirinov666000@gmail.com>
2023-12-11 20:25:23 +0400

    Factory Pattern Added

---
+++ AutomobileFactory/AutomobileFactory/Entities/Classes/SUV.cs
@@ -0,0 +1,16 @@
+﻿using AutomobileFactory.Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomobileFactory.Entities.Classes;
+
+public class SUV : IAutomobile
+{
+    public string Name { get; set; }
+    public string HP { get; set; }
+    public string Speed { get; set; }
+    public string Seats { get; set; }
+}
---
+++ AutomobileFactory/AutomobileFactory/Factories/Interfaces/IAutomobileFactory.cs
@@ -0,0 +1,13 @@
+﻿using AutomobileFactory.Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomobileFactory.Factories.Interfaces;
+
+public interface IAutomobileFactory
+{
+    public T CreateAutomobile<T>() where T : IAutomobile;
+}
